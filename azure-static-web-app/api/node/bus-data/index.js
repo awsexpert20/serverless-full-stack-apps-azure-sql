d67d3d639ca0a4395aab962a938f50f3936d3142
Yaseen M <yaseen_gem@yahoo.com>
2022-04-05 00:48:55 +1000

    5

--- azure-static-web-app/api/node/bus-data/index.js
+++ azure-static-web-app/api/node/bus-data/index.js
@@ -5,7 +5,8 @@ const AZURE_CONN_STRING = process.env["AzureSQLConnectionString"];
 module.exports = async function (context, req) {    
     const pool = await sql.connect(AZURE_CONN_STRING);    
     context.log ('Pool :', pool);
-    
+    context.log ("bus data function route id :", req.query.rid);
+    context.log ("bus data function group id :", req.query.gid);
     const busData = await pool.request()
         .input("routeId", sql.Int, parseInt(req.query.rid))
         .input("geofenceId", sql.Int, parseInt(req.query.gid))
