f25b45ff76fc7cd0b4b917df9071e2391e43ae75
Yaseen M <yaseen_gem@yahoo.com>
2022-04-05 00:45:21 +1000

    4

--- azure-static-web-app/api/node/bus-data/index.js
+++ azure-static-web-app/api/node/bus-data/index.js
@@ -4,12 +4,14 @@ const AZURE_CONN_STRING = process.env["AzureSQLConnectionString"];
 
 module.exports = async function (context, req) {    
     const pool = await sql.connect(AZURE_CONN_STRING);    
-
+    context.log ('Pool :', pool);
+    
     const busData = await pool.request()
         .input("routeId", sql.Int, parseInt(req.query.rid))
         .input("geofenceId", sql.Int, parseInt(req.query.gid))
         .execute("web.GetMonitoredBusData");        
-
+  
+    context.log ('Context Response :', context.res);
     context.res = {        
         body: JSON.parse(busData.recordset[0]["locationData"])
     };
